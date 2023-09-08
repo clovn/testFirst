a19366873d1ff076f3b202b8ab6f53ba63ae5523
clovn <haliullin101@mail.ru>
2023-09-08 15:33:10 +0300

    added reader

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class HelloWorld{
-	public static void main(String[] args){
-		System.out.println("Hello world");
+	public static void main(String[] args) throws IOException{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Hello " + reader.readLine());
+
 	}
 }
